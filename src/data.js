a3560f522725bb623c1908c9293f20f3bd355df4
Efrain Arcadio <103285774+MiloDeSkorpio@users.noreply.github.com>
2023-05-18 17:52:45 -0500

    Data

---
+++ src/data.js
@@ -0,0 +1,51 @@
+// import imagenes
+
+export const header = {
+  logo: 'aqui va el logo',
+  btnText: 'Contactanos'
+}
+export const nav = [
+  { name: 'Mision', href:'#mision' },
+  { name: 'Vision', href: '#vision' },
+  { name: 'Servicios', href: '#servicios' }
+]
+export const hero = {
+
+}
+
+export const mision = {
+  title: 'Nuestra Misión',
+  subtitle: 'Proporcionar servicios legales de alta calidad y asesoría jurídica personalizada a nuestros clientes, basados en la ética profesional, el compromiso y la excelencia, con el fin de proteger sus derechos e intereses, resolver conflictos y alcanzar soluciones efectivas a sus necesidades legales, aportando valor a la sociedad y construyendo relaciones duraderas y de confianza con nuestros clientes',
+  btnLink: '#vision',
+  image: 'aqui va la image'
+}
+
+export const vision = {
+  title: 'Nuestra Vision',
+  subtitle: 'Ser reconocidos como líderes en la prestación de servicios legales de alta calidad y excelencia, en constante evolución y mejora, con un enfoque en la satisfacción del cliente y la solución efectiva de sus necesidades jurídicas, destacándonos por nuestra ética profesional, compromiso y capacidad de innovación y adaptación a un entorno legal en constante cambio. Aspiramos a ser un despacho jurídico de referencia, que contribuya al desarrollo de la sociedad y que sea un lugar de trabajo inspirador y motivador para nuestros profesionistas a nivel nacional, dejando una marca en el hito hístorico del pais',
+  btnLink: '#servicios',
+  image: 'aqui va la image'
+}
+
+export const servicios = {
+  title: 'Nuestros Servicios',
+  catalogo: [
+    { name: 'Licencia de Construción' },
+    { name: 'Licencia de Uso de Suelo' },
+    { name: 'Licencia de Demolición' },
+    { name: 'Tramites relativos a construcción' },
+  ]
+
+}
+export const contacto = {
+  title: 'Contactanos',
+  image: 'aqui va la image'
+}
+
+export const social = {
+  title: 'Redes Sociales',
+  redes: [
+    {name: 'Facebook', href: ''},
+    {name: 'WhatsApp', href: ''},
+  ]
+}
