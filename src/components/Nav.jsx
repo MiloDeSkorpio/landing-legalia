ba82b74585c4fb0db556e5324a824505444be42e
Efrain Arcadio <103285774+MiloDeSkorpio@users.noreply.github.com>
2023-05-24 06:50:30 -0500

    StyleColors

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,12 +5,12 @@ import { nav } from '../data.js'
 const Nav = () => {
   return (
     <div className='bg-white w-full h-full'>
-    <ul className='h-full flex flex-row justify-center items-center gap-x-8'>
+    <ul className='h-full flex flex-row justify-center items-center  gap-x-8'>
       {nav.map((item, index) => {
         //destrucure item
         const { href, name } = item
         return (
-          <li key={index}>
+          <li key={index} className='hover:bg-[#d1ae6c] hover:shadow-md p-1 rounded-sm '>
             <a className='link text-black text-xl' href={href}>
               {name}
             </a>
