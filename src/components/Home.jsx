e9d71f20af9b834a40f8a8c6adacbebfc9de4f43
Efrain Arcadio <103285774+MiloDeSkorpio@users.noreply.github.com>
2023-05-28 05:51:43 -0500

    Home-1.4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,25 @@
-import {React, useRef, useEffect} from 'react'
-//import data
+import { React, useRef, useEffect } from 'react';
+// import data
 import { hero } from '../data';
+
 const Home = () => {
   const videoRef = useRef(null);
+
   useEffect(() => {
     videoRef.current.play();
   }, []);
-  const { video} = hero
+
+  const { video } = hero;
+
   return (
     <section className='bg-white section flex items-center justify-center min-h-[900px]'>
-      <div 
-        className='w-5/6 lg:w-5/12'
-        data-aos='zoom-out'
-        data-aos-delay='1000'      
-      >
-        <video ref={videoRef}>
-          <source src={video} type="video/mp4" />
+      <div className='w-5/6 lg:w-5/12' data-aos='zoom-out' data-aos-delay='1000'>
+        <video ref={videoRef} muted>
+          <source src={video} type='video/mp4' />
         </video>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
