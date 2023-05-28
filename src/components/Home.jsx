cc7e9c87b3e38c3906aba711f53113b1eeebdd0b
Efrain Arcadio <103285774+MiloDeSkorpio@users.noreply.github.com>
2023-05-28 05:53:51 -0500

    Home-1.5

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   return (
     <section className='bg-white section flex items-center justify-center min-h-[900px]'>
       <div className='w-5/6 lg:w-5/12' data-aos='zoom-out' data-aos-delay='1000'>
-        <video ref={videoRef} muted>
+        <video ref={videoRef} muted loop>
           <source src={video} type='video/mp4' />
         </video>
       </div>
